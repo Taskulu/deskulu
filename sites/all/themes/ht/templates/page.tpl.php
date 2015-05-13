17b2ff800b054e887d4d8cfbeac4e548ef24eea3
Farhad Hedayati Fard <hf.farhad@gmail.com>
2015-05-13 16:14:47 +0430

    Style update

--- sites/all/themes/ht/templates/page.tpl.php
+++ sites/all/themes/ht/templates/page.tpl.php
@@ -163,9 +163,7 @@
         <?php if (!empty($tabs)): ?>
           <?php print render($tabs); ?>
         <?php endif; ?>
-        <?php if (!empty($page['help'])): ?>
-          <?php print render($page['help']); ?>
-        <?php endif; ?>
+
         <?php if (!empty($action_links)): ?>
           <ul class="action-links"><?php print render($action_links); ?></ul>
         <?php endif; ?>
