03afd06dc64ceeb177c55bf728b2a4be036660e7
Farhad Hedayati Fard <hf.farhad@gmail.com>
2015-06-25 12:56:50 +0430

    Fix ticket type dropdown on ticket page.

--- sites/all/themes/ht/js/ticket-reply-form.js
+++ sites/all/themes/ht/js/ticket-reply-form.js
@@ -1,6 +1,7 @@
 (function ($) {
     Drupal.behaviors.ticket_form_reply = {
         attach: function(context, settings) {
+            $('body.node-type-ticket .field-name-field-ticket-type select').select2().removeClass('form-control');
             $('.comment-form .title-wrapper ul li.cc a').on('click', function(e) {
                 $(this).parent().hide();
                 $('#field_email_cc_wrapper').removeClass('hidden');
